3666377f38a662558d605c1bc0352cde13a191e8
thithi0001 <ledinhthi0002@gmail.com>
2024-09-30 17:02:46 +0700

    setup tile

--- src/main/java/tile/Tile.java
+++ src/main/java/tile/Tile.java
@@ -6,4 +6,9 @@ public class Tile {
 
     public BufferedImage image = null;
     public boolean collision = false;
+    public boolean destructible = false;
+    /*
+    * bomb can destroy destructible tiles and objects
+    * if an entity is in the intangible state, it can pass through destructible tiles and objects
+    * */
 }
