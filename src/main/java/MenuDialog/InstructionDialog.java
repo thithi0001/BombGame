d7422d676cc71244120e3680c0c29739931ffae4
thithi0001 <ledinhthi0002@gmail.com>
2024-10-27 23:24:12 +0700

    InstructionDialog

--- src/main/java/MenuDialog/InstructionDialog.java
+++ src/main/java/MenuDialog/InstructionDialog.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 import MenuSetUp.MyButton;
+import res.LoadResource;
 
 public class InstructionDialog extends SuperDialog {
 
@@ -25,12 +26,27 @@ public class InstructionDialog extends SuperDialog {
 
     void setContent() {
         JTextArea content = new JTextArea();
-        content.setSize(350, 325);
-        content.setLocation(75, 75);
+        content.setSize(450, 325);
+        content.setLocation(45, 75);
         content.setEditable(false);
         content.setLineWrap(true);//DISPLAY MULTIPLE LINE
         content.setOpaque(false);
         getContentPane().add(content);
+        content.setFont(LoadResource.instructionContent);
+        content.setText("""
+                CONTROL:
+                + W: Go up
+                + A: Go left
+                + S: Go down
+                + D: Go right
+                + R: Activate time bomb
+                ------------------------------
+                RULE:
+                + Eliminate all monster in map
+                + Don't touch any monster
+                + Don't get hit by bomb
+                
+                """);
     }
 
 }
